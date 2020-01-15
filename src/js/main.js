06690933426fc22e1a010a8c4cb5b64f25ea8ae7
Matthiasgdr <spam68130@gmail.com>
2020-01-15 17:15:35 +0100

    Main

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,5 @@ console.log(window.innerHeight);
 
 window.addEventListener('scroll', () => {
     
-    parallax.style.backgroundPosition = `50% ${50 + (((window.scrollY*50)/window.innerHeight)*1.2)}%`
+    parallax.style.backgroundPosition = `50% ${50 - (((window.scrollY*50)/window.innerHeight)*1.5)}%`
 })
