c2704038a5b71b6567011a2bd62e5f9f33d3b62f
Wizeeps <spam68130@gmail.com>
2020-01-15 15:27:54 +0100

    Header

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+let parallax = document.querySelector('.parallax')
+
+console.log(window.innerHeight);
+
+
+window.addEventListener('scroll', () => {
+    
+    parallax.style.backgroundPosition = `50% ${50 + (((window.scrollY*50)/window.innerHeight)*1.2)}%`
+})
